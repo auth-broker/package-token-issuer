6c1cbbdc5c65bfcca45d45f82f1211cef25eb826
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2025-10-12 20:23:40 +1100

    test: use async methods

--- tests/ab_core/token_issuer/token_issuers/test_pkce.py
+++ tests/ab_core/token_issuer/token_issuers/test_pkce.py
@@ -1,8 +1,11 @@
+import pytest
+
 from ab_core.auth_client.oauth2.client.pkce import (
     PKCEOAuth2Client,
 )
 from ab_core.auth_client.oauth2.schema.oidc import OIDCConfig
 from ab_core.auth_client.oauth2.schema.refresh import RefreshTokenRequest
+from ab_core.auth_client.oauth2.schema.token import OAuth2Token
 from ab_core.auth_flow.oauth2.flow.impersonation import (
     ImpersonationOAuth2Flow,
 )
@@ -16,7 +19,8 @@ from ab_core.token_issuer.token_issuers.pkce import (
 )
 
 
-def test_wemoney_authenticate(tmp_cache_sync_session):
+@pytest.mark.asyncio
+async def test_wemoney_authenticate(tmp_cache_async_session):
     issuer = PKCEOAuth2TokenIssuer(
         identity_provider="Google",
         response_type="code",
@@ -31,7 +35,7 @@ def test_wemoney_authenticate(tmp_cache_sync_session):
         ),
         oauth2_flow=ImpersonationOAuth2Flow(
             idp_prefix="https://wemoney.auth.ap-southeast-2.amazoncognito.com/oauth2/idpresponse",
-            timeout=9999999,
+            timeout=600 * 1000,  # 600 seconds
             impersonator=PlaywrightCDPBrowserlessImpersonator(
                 cdp_endpoint="wss://browserless.matthewcoulter.dev/?stealth=true&blockAds=true&ignoreHTTPSErrors=true&timezoneId=Australia/Sydney",
                 cdp_gui_service=CDPGUIService(
@@ -43,17 +47,22 @@ def test_wemoney_authenticate(tmp_cache_sync_session):
         ),
     )
 
-    # 1. test getting the token
-    for token in issuer.authenticate(cache_session=tmp_cache_sync_session):
-        print(token)
+    # 1) test getting the token
+    token: OAuth2Token | None = None
+    async for item in issuer.authenticate_async(cache_session=tmp_cache_async_session):
+        if isinstance(item, OAuth2Token):
+            token = item
+    assert token is not None
 
-    # 2. refresh the token
-    refreshed_token = next(
-        issuer.refresh(
-            RefreshTokenRequest(refresh_token=token.refresh_token),
-            cache_session=tmp_cache_sync_session,
-        )
-    )
+    # 2) refresh the token
+    refreshed_token: OAuth2Token | None = None
+    async for t in issuer.refresh_async(
+        RefreshTokenRequest(refresh_token=token.refresh_token),
+        cache_session=tmp_cache_async_session,
+    ):
+        refreshed_token = t
+        break
+    assert refreshed_token is not None
 
-    # 3. Ensure the refresh token persists
+    # 3) Ensure the refresh token persists
     assert refreshed_token.refresh_token == token.refresh_token
